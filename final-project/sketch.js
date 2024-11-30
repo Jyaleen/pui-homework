7b3d0a2474e79dbacafa649e241cdd5d30c64897
Jyaleen <jjbean125@gmail.com>
2024-11-30 15:57:26 -0500

    p5.js eyes

---
+++ final-project/sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+    let myCanvas = createCanvas(300, 300);
+    myCanvas.parent('myContainer');
+    colorMode(HSB);
+    angleMode(DEGREES);
+}
+
+function draw() {
+
+    let leftX = 150;
+    let leftY = 190;
+    let leftAngle = atan2(mouseY - leftY, mouseX - leftX);
+
+    push();
+    translate(leftX, leftY);
+    fill(255);
+    ellipse(0, 0, 50, 50);
+    rotate(leftAngle);
+    fill(0);
+    ellipse(12.5, 0, 25, 25);
+    pop();
+
+    let rightX = 250;
+    let rightY = 190;
+    let rightAngle = atan2(mouseY - rightY, mouseX - rightX);
+
+    push();
+    translate(rightX, rightY);
+    fill(255);
+    ellipse(0, 0, 50, 50);
+    rotate(rightAngle);
+    fill(0);
+    ellipse(12.5, 0, 25, 25);
+    pop();
+}
